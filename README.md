1f22602396532050f4695f7d6c2129bdebadae40
ajbitus <ajaykpradhan61@gmail.com>
2023-10-20 13:39:28 +0530

    updated files

---
+++ README.md
@@ -0,0 +1,8 @@
+# JS Playground
+## By [@ajbitus](https://github.com/ajbitus)
+### 1 - analog-clock (Source: 🔗[https://codepen.io/vaskopetrov/pen/yVEXjz](https://codepen.io/vaskopetrov/pen/yVEXjz))
+![analog-clock](./analog-clock/output.jpg "analog-clock")
+### 2 - image-nodes (Source: 🔗[https://cssdeck.com/labs/full/image-nodes](https://cssdeck.com/labs/full/image-nodes))
+![image-nodes](./image-nodes/output.jpg "image-nodes")
+### 3 - matrix-effect (Source: 🔗[https://jsfiddle.net/henam/yfnvnqy5/](https://jsfiddle.net/henam/yfnvnqy5/))
+![matrix-effect](./matrix-effect/output.jpg "matrix-effect")
--- prepare.js
+++ prepare.js
@@ -1,6 +1,9 @@
-const { readdir } = require("node:fs/promises");
+const { readdir, readFile, writeFile } = require("node:fs/promises");
 const path = require("node:path");
-// const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer");
+const htmlCreator = require("html-creator");
+
+const excludedDirs = [".git", "node_modules"];
 
 const timeout = (ms) => {
  return new Promise((resolve) => setTimeout(resolve, ms));
@@ -9,68 +12,182 @@ const timeout = (ms) => {
 const listDirectories = async (inputPath) => {
  const directories = (await readdir(inputPath, { withFileTypes: true }))
   .filter((dirent) => dirent.isDirectory())
-  .filter((dir) => [".git", "node_modules"].indexOf(dir.name) < 0)
+  .filter((dir) => excludedDirs.indexOf(dir.name) < 0)
   .map((dir) => dir.name);
 
  return directories;
 };
 
+const loadPageAndSaveScreenshot = async (browser, project) => {
+ // Create a new page
+ const page = await browser.newPage();
+
+ //   page
+ //    .on("console", (message) =>
+ //     console.log(
+ //      `${message.type().substr(0, 3).toUpperCase()} ${message.text()}`
+ //     )
+ //    )
+ //    .on("pageerror", ({ message }) => console.log(message))
+ //    .on("response", (response) =>
+ //     console.log(`${response.status()} ${response.url()}`)
+ //    )
+ //    .on("requestfailed", (request) =>
+ //     console.log(`${request.failure().errorText} ${request.url()}`)
+ //    );
+
+ // Set viewport width and height
+ await page.setViewport({ width: 800, height: 600 });
+
+ const website_url = `file:///C:/Users/ajayk/Projects/Code/js-playground/${project}/index.html`;
+
+ // Open URL in current page
+ await page.goto(website_url, { waitUntil: "networkidle0" });
+
+ await timeout(3000);
+
+ //  console.log(await page.content());
+
+ // Capture screenshot
+ await page.screenshot({
+  path: `./${project}/output.jpg`,
+ });
+};
+
+// Main method for iteration for projects
 (async () => {
- //  // Create a browser instance
- //  const browser = await puppeteer.launch({ headless: "new" });
+ const projectDirs = await listDirectories(__dirname);
 
- //  // Create a new page
- //  const page = await browser.newPage();
+ // Create a browser instance
+ const browser = await puppeteer.launch({ headless: "new" });
 
- //  page
- //   .on("console", (message) =>
- //    console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`)
- //   )
- //   .on("pageerror", ({ message }) => console.log(message))
- //   .on("response", (response) =>
- //    console.log(`${response.status()} ${response.url()}`)
- //   )
- //   .on("requestfailed", (request) =>
- //    console.log(`${request.failure().errorText} ${request.url()}`)
- //   );
+ const indexHTML = new htmlCreator([
+  {
+   type: "head",
+   content: [
+    {
+     type: "title",
+     content: "JS Playground | AJBitus",
+    },
+    {
+     type: "link",
+     attributes: { rel: "stylesheet", href: "./index.css" },
+    },
+    {
+     type: "meta",
+     attributes: {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1.0, user-scalable=0",
+     },
+    },
+    {
+     type: "script",
+     attributes: {
+      async: true,
+      src: "https://www.googletagmanager.com/gtag/js?id=G-LRM107MFY2",
+     },
+    },
+    {
+     type: "script",
+     content:
+      "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-LRM107MFY2');",
+    },
+   ],
+  },
+  {
+   type: "body",
+   content: [
+    { type: "header", content: [{ type: "h1", content: "JS Playground" }] },
+    {
+     type: "main",
+     content: [
+      {
+       type: "div",
+       attributes: { class: "projects" },
+      },
+     ],
+    },
+    {
+     type: "footer",
+     content: [{ type: "h4", content: `By @ajbitus` }],
+    },
+   ],
+  },
+ ]);
 
- //  // Set viewport width and height
- //  await page.setViewport({ width: 800, height: 600 });
+ let readmeFile =
+   "# JS Playground\n## By [@ajbitus](https://github.com/ajbitus)\n",
+  projectIndex = 0;
 
- //  const website_url = "file:///C:/Users/ajayk/Projects/Code/js-playground/matrix-effect/index.html";
+ for await (const project of projectDirs) {
+  await loadPageAndSaveScreenshot(browser, project);
 
- //  // Open URL in current page
- //  await page.goto(website_url, { waitUntil: "networkidle0" });
+  const sourceLink = await readFile(path.join(__dirname, project, "LINK"));
 
- //  await timeout(3000);
+  indexHTML.document.addElementToClass("projects", {
+   type: "div",
+   attributes: { id: `project-${project}`, class: "project" },
+   content: [
+    {
+     type: "a",
+     attributes: { href: `./${project}/`, class: "project-img-link" },
+     content: [
+      {
+       type: "img",
+       attributes: {
+        src: `./${project}/output.jpg`,
+        alt: project,
+        class: "project-img",
+       },
+      },
+     ],
+    },
+   ],
+  });
 
- // //  console.log(await page.content());
+  indexHTML.document.addElementToId(`project-${project}`, {
+   type: "a",
+   attributes: { href: `./${project}/`, class: "project-title-link" },
+   content: [
+    {
+     type: "h3",
+     attributes: { class: "project-title" },
+     content: project,
+    },
+   ],
+  });
 
- //  // Capture screenshot
- //  await page.screenshot({
- //   path: "./analog-clock/screenshot.jpg",
- //  });
+  indexHTML.document.addElementToId(`project-${project}`, {
+   type: "a",
+   attributes: {
+    href: sourceLink,
+    target: "_blank",
+    class: "project-ext-link",
+   },
+   content: [
+    {
+     type: "h4",
+     attributes: { id: `project-${project}-ext-link`, class: "project-ext" },
+     content: `🔗 ${sourceLink}`,
+    },
+   ],
+  });
 
- //  // Close the browser instance
- //  await browser.close();
- // });
+  readmeFile += `### ${
+   projectIndex + 1
+  } - ${project} (Source: 🔗[${sourceLink}](${sourceLink}))\n![${project}](./${project}/output.jpg "${project}")\n`;
 
- const directories = await listDirectories(__dirname);
+  projectIndex++;
+ }
 
- console.log(directories);
- console.log(path.resolve(path.join(__dirname, "analog-clock", "index.html")));
+ await indexHTML.renderHTMLToFile(path.join(__dirname, "index.html"), {
+  htmlTagAttributes: {
+   lang: "en",
+  },
+ });
+
+ await writeFile("./README.md", readmeFile);
+
+ // Close the browser instance
+ await browser.close();
 })();
-
-// listDirectories(__dirname).then((directories) => {
-//  console.log(directories);
-
-// [
-//   '.git',
-//   '.vscode',
-//   'controllers',
-//   'node_modules',
-//   'pages',
-//   'routes',
-//   'src',
-//   'views'
-// ]
